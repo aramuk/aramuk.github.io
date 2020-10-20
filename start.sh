aba805ef86341f29ea48c8c2e724cdf609485f40
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2020-10-19 22:28:05 -0700

    Improved start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 PORT=${1:-"8080"}
-wasm-pack build --target web --out-name wasm --out-dir ./static
-cd ./static && python3 -m http.server $PORT
+COMPILER_ERRORS=$(wasm-pack build --target web --out-name wasm --out-dir ./static 2>&1 | grep 'error: could not compile `aramuk-web`.')
+if [[ -z "$COMPILER_ERRORS" ]]; then
+    cd ./static && python3 -m http.server $PORT &> /dev/null &
+    pid=$!
+    sleep 1
+    open "http://localhost:$PORT"
+    kill "${pid}"
+fi
