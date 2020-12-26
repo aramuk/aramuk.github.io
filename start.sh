bf954e54a1ae73ba4bd693b0e5fa6c7d0de791e2
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2020-12-25 21:46:34 -0800

    Make start.sh run in the foreground

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PORT=${1:-"8080"}
+
 COMPILER_ERRORS=$(wasm-pack build --target web --out-name wasm --out-dir ./static 2>&1 | grep 'error: could not compile `aramuk-web`.')
+ps aux | grep http | awk '{print $2}' | xargs kill &> /dev/null
 if [[ -z "$COMPILER_ERRORS" ]]; then
-    cd ./static && python3 -m http.server $PORT &> /dev/null &
-    pid=$!
-    sleep 1
+    cd ./static && python3 -m http.server ${PORT}
     open "http://localhost:${PORT}"
-    kill "${pid}"
+else 
+    echo "${COMPILER_ERRORS}"
 fi
