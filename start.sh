e7003119a994e30001dda3c08d593ef79e24fadb
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2021-01-01 22:01:41 -0800

    Update running script and meta tags

--- start.sh
+++ start.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
+set -Eeuo pipefail
+PID=""
+
+cleanup () {
+    if [[ -nz "$PID" ]]; then
+        kill ${PID}
+    fi
+    trap - SIGINT SIGTERM ERR EXIT
+    exit
+}
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
 PORT=${1:-"8080"}
 
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+error () {
+    printf "Error: %s\n" "$*" >&2
+}
+
 COMPILER_ERRORS=$(wasm-pack build --target web --out-name wasm --out-dir ./static 2>&1 | grep 'error: could not compile `aramuk-web`.')
-ps aux | grep 'http.server' | awk '{print $2}' | xargs kill &> /dev/null
 if [[ -z "$COMPILER_ERRORS" ]]; then
-    cd ./static && python3 -m http.server ${PORT} &
+    PID=$(cd ./static && python3 -m http.server ${PORT} &>/dev/null && echo $!)
     sleep 1
-    open "http://localhost:${PORT}"
-else 
-    echo "${COMPILER_ERRORS}"
+    LINK="http://localhost:${PORT}"
+    echo "Project running on ${LINK}"
+    open "${LINK}"
+else
+    error "failed to compile ${basename ${script_dir}}" 
 fi
